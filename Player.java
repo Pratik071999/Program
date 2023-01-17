00c69c5e37083a37dc724f6b6d1ff058596871b7
Pratik071999 <122912397+Pratik071999@users.noreply.github.com>
2023-01-18 00:17:53 +0530

    Add files via upload

---
+++ Player.java
@@ -0,0 +1,32 @@
+package com.monocept.test;
+
+public class Player {
+	public String name;
+	public int rounds = 0;
+	public int score;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getRounds() {
+		return rounds;
+	}
+
+	public void setRounds(int rounds) {
+		this.rounds = rounds;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+}
